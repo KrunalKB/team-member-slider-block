4339d89ab28ec53d0befdaaa9c67a2fef84c3a67
Krunal Bhimajiyani <krunalbhimajiyani11@gmail.com>
2023-01-31 00:26:35 +0530

    improve main file

--- team-member-slider-block.php
+++ team-member-slider-block.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * Plugin Name:       Team Member Slider Block
- * Description:       A team members slider gutenberg block.
- * Requires at least: 5.9
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            Krunal Bhimajiyani
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       team-members
+ * Team Member Slider Block
  *
- * @package           create-block
+ * @package           Team Member Slider Block
+ * @author            Harit Panchal
+ * @copyright         2022 Harit Panchal
+ * @license           GPL-2.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Team Member Slider Block
+ * Description:       A team member's gutenberg block with column and slider layout.
+ * Requires at least: 6.1
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            Krunal Bhimajiyani
+ * Author URI:        https://profiles.wordpress.org/krunal265/
+ * License:           GPL v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       team-members
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_team_member_slider_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
@@ -29,13 +29,13 @@ add_action(
 	'wp_enqueue_scripts',
 	function () {
 
-		wp_enqueue_script( 'tmsb-slickerjs', plugins_url( 'assets/js/slick.js', __FILE__ ), array(), null, true );
+		wp_enqueue_script( 'tmsb-slickerjs', plugins_url( 'assets/js/slick.js', __FILE__ ), array(), '1.0.0', true );
 
-		wp_enqueue_script( 'tmsb-slickerminjs', plugins_url( 'assets/js/slick.min.js', __FILE__ ), array(), null, true );
+		wp_enqueue_script( 'tmsb-slickerminjs', plugins_url( 'assets/js/slick.min.js', __FILE__ ), array(), '1.0.0', true );
 
-		wp_enqueue_style( 'tmsb-slickercss', plugins_url( 'assets/css/slick.css', __FILE__ ), array(), false, 'all' );
+		wp_enqueue_style( 'tmsb-slickercss', plugins_url( 'assets/css/slick.css', __FILE__ ), array(), '1.0.0', 'all' );
 
-		wp_enqueue_style( 'tmsb-slickerthemecss', plugins_url( 'assets/css/slick-theme.css', __FILE__ ), array(), false, 'all' );
+		wp_enqueue_style( 'tmsb-slickerthemecss', plugins_url( 'assets/css/slick-theme.css', __FILE__ ), array(), '1.0.0', 'all' );
 
 		wp_enqueue_script( 'tmsb-sliderjs', plugins_url( 'assets/js/slickerslider.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
 	}
@@ -44,13 +44,13 @@ add_action(
 	'admin_enqueue_scripts',
 	function () {
 
-		wp_enqueue_style( 'tmsb-slickercss', plugins_url( 'assets/css/slick.css', __FILE__ ), array(), false, 'all' );
+		wp_enqueue_style( 'tmsb-slickercss', plugins_url( 'assets/css/slick.css', __FILE__ ), array(), '1.0.0', 'all' );
 
-		wp_enqueue_style( 'tmsb-slickerthemecss', plugins_url( 'assets/css/slick-theme.css', __FILE__ ), array(), false, 'all' );
+		wp_enqueue_style( 'tmsb-slickerthemecss', plugins_url( 'assets/css/slick-theme.css', __FILE__ ), array(), '1.0.0', 'all' );
 
-		wp_enqueue_script( 'tmsb-slickerjs', plugins_url( 'assets/js/slick.js', __FILE__ ), array(), null, true );
+		wp_enqueue_script( 'tmsb-slickerjs', plugins_url( 'assets/js/slick.js', __FILE__ ), array(), '1.0.0', true );
 
-		wp_enqueue_script( 'tmsb-slickerminjs', plugins_url( 'assets/js/slick.min.js', __FILE__ ), array(), null, true );
+		wp_enqueue_script( 'tmsb-slickerminjs', plugins_url( 'assets/js/slick.min.js', __FILE__ ), array(), '1.0.0', true );
 
 	}
 );
